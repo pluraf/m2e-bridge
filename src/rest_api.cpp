97874390fac0de6b74dd7db00a7cd8dfe989c918
Konstantin Tyurin <konstantin@pluraf.com>
2024-09-23 21:33:43 +0200

    Apply proper indentation

--- src/rest_api.cpp
+++ src/rest_api.cpp
@@ -15,86 +15,89 @@
 
 
 class AuthHandler:public CivetAuthHandler {
-    public:
-        AuthHandler(std::string* public_key):public_key_(public_key) {}
+public:
+    AuthHandler(std::string* public_key):public_key_(public_key) {}
 
-        bool authorize(CivetServer *server, struct mg_connection *conn) override {
-            const char* auth_token = mg_get_header(conn, "Authorization");
-            if (auth_token != NULL && strlen(auth_token) > 7) {
-                const char *token = auth_token + 7;  // Skip "Bearer "
+    bool authorize(CivetServer *server, struct mg_connection *conn) override {
+        const char* auth_token = mg_get_header(conn, "Authorization");
+        if (auth_token != NULL && strlen(auth_token) > 7) {
+            const char *token = auth_token + 7;  // Skip "Bearer "
 
-                if (jwt_verify(token, *public_key_)) {
-                    return 1;
-                }
+            if (jwt_verify(token, *public_key_)) {
+                return 1;
             }
+        }
 
-            mg_send_http_error(conn, 401, "");
+        mg_send_http_error(conn, 401, "");
+        return 0;
+    }
+
+private:
+    std::string* public_key_;
+};
+
+
+class CreatePipelineHandler:public CivetHandler {
+public:
+    bool handlePost(CivetServer *server, struct mg_connection * conn) override {
+        std::string pipeid;
+        json pipeline_data;
+
+        if(parse_request_body(conn, pipeid, pipeline_data) != 0) {
+            mg_send_http_error(conn, 400, "Could not parse request!");
             return 0;
         }
 
-    private:
-        std::string* public_key_;
-};
-
-class CreatePipelineHandler:public CivetHandler {
-    public:
-        bool handlePost(CivetServer *server, struct mg_connection * conn) override {
-            std::string pipeid;
-            json pipeline_data;
-
-            if(parse_request_body(conn, pipeid, pipeline_data) != 0) {
-                mg_send_http_error(conn, 400, "Could not parse request!");
-                return 0;
-            }
-        
-            if(gc.add_pipeline(pipeid, pipeline_data) != 0) {
-                mg_send_http_error(conn, 500, "Failed to add pipeline!");
-                return 0;
-            }
-
-            return 1;
+        if(gc.add_pipeline(pipeid, pipeline_data) != 0) {
+            mg_send_http_error(conn, 500, "Failed to add pipeline!");
+            return 0;
         }
+
+        return 1;
+    }
 };
+
 
 class EditPipelineHandler:public CivetHandler {
-    public:
-        bool handlePost(CivetServer *server, struct mg_connection * conn) override {
-            std::string pipeid;
-            json pipeline_data;
+public:
+    bool handlePost(CivetServer *server, struct mg_connection * conn) override {
+        std::string pipeid;
+        json pipeline_data;
 
-            if(parse_request_body(conn, pipeid, pipeline_data) != 0) {
-                mg_send_http_error(conn, 400, "Could not parse request!");
-                return 0;
-            }
-        
-            if(gc.edit_pipeline(pipeid, pipeline_data) != 0) {
-                mg_send_http_error(conn, 500, "Failed to edit pipeline!");
-                return 0;
-            }
-
-            return 1;
+        if(parse_request_body(conn, pipeid, pipeline_data) != 0) {
+            mg_send_http_error(conn, 400, "Could not parse request!");
+            return 0;
         }
+
+        if(gc.edit_pipeline(pipeid, pipeline_data) != 0) {
+            mg_send_http_error(conn, 500, "Failed to edit pipeline!");
+            return 0;
+        }
+
+        return 1;
+    }
 };
 
-class DeletePipelineHandler:public CivetHandler {
-    public:
-        bool handlePost(CivetServer *server, struct mg_connection * conn) override {
-            std::vector<std::string> pipeline_ids;
 
-            if(parse_pipeline_ids(conn, pipeline_ids) != 0) {
-                mg_send_http_error(conn, 400, "Could not parse request!");
+class DeletePipelineHandler:public CivetHandler {
+public:
+    bool handlePost(CivetServer *server, struct mg_connection * conn) override {
+        std::vector<std::string> pipeline_ids;
+
+        if(parse_pipeline_ids(conn, pipeline_ids) != 0) {
+            mg_send_http_error(conn, 400, "Could not parse request!");
+            return 0;
+        }
+
+        for(const auto &pipeid : pipeline_ids) {
+            if(gc.delete_pipeline(pipeid) != 0) {
+                mg_send_http_error(conn, 500, "Failed to delete pipeline!");
                 return 0;
             }
-
-            for(const auto &pipeid : pipeline_ids) {
-                if(gc.delete_pipeline(pipeid) != 0) {
-                    mg_send_http_error(conn, 500, "Failed to delete pipeline!");
-                    return 0;
-                }
-            }
-        
-            return 1;
         }
+
+        return 1;
+    }
 };
 
 
@@ -122,6 +125,7 @@ CivetServer* start_server() {
 
     return server;
 }
+
 
 void stop_server(CivetServer* server) {
     if (server) {
