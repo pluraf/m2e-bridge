6a0b139c29d3d0273beb3ec3e4b8a95c1cfaec05
gokce <gokce@pluraf.com>
2024-12-10 14:46:43 +0100

    Add connector IN mode

--- src/connectors/email_connector.h
+++ src/connectors/email_connector.h
@@ -38,26 +38,24 @@ IN THE SOFTWARE.
 #include "connector.h"
 #include "database.h"
 
-const int  SMTP_PORT = 587;  //Default SMTP port 587
+const int  SMTP_PORT = 587;  // Default SMTP port
+const int IMAP_PORT = 993;  // Default IMAP port
 
 
 class EmailConnector: public Connector {
     friend class EmailConnectorTests;
 private:
     std::string smtp_server_;
-    std::string to_;
+    std::string imap_server_;
+    std::string address_;
     int smtp_port_;
+    int imap_port_;
     std::string username_;  // Email address to send mail from
     std::string password_;  // Password for pluraf mail address
     std::string authbundle_id_;
 
     CURL * curl;
     CURLcode res = CURLE_OK;
-
-    struct upload_status {
-        int lines_read;
-        std::vector<std::string>* email_content;
-    };
 
     void parse_authbundle(){
         Database db;
@@ -72,39 +70,10 @@ private:
         }
     }
 
-public:
-    EmailConnector(std::string pipeid, ConnectorMode mode_, json const & json_descr):
-            Connector(pipeid, mode_, json_descr){
-        if(mode_ != ConnectorMode::OUT) {
-            throw std::runtime_error("Email connector only supports OUT mode");
-        }
-
-        try{
-            authbundle_id_ = json_descr.at("authbundle_id").get<std::string>();
-            parse_authbundle();
-        }catch(json::exception){
-            throw std::runtime_error("authbundle_id cannot be null for email connector");
-        }
-
-        try {
-            to_ = json_descr.at("to").get<std::string>();
-        }catch(json::exception) {
-            throw std::runtime_error("Destination adress cannot be null for email connector");
-        }
-
-        try {
-            smtp_server_ = json_descr.at("smtp_server").get<std::string>();
-        }catch(json::exception) {
-            throw std::runtime_error("Smtp server cannot be null for email connector");
-        }
-
-        try {
-            smtp_port_ = json_descr.at("smtp_port").get<int>();
-        }catch(json::exception) {
-            smtp_port_ = SMTP_PORT;
-        }
-
-    }
+    struct upload_status{
+        int lines_read;
+        std::vector<std::string>* email_content;
+    };
 
     static size_t payload_source(void* ptr, size_t size, size_t nmemb, void *userp){
         struct upload_status *upload_ctx = (struct upload_status *)userp;
@@ -126,12 +95,64 @@ public:
         return 0;
     }
 
+    static size_t write_callback(void *ptr, size_t size, size_t nmemb, void *userp){
+        std::string *data = static_cast<std::string*>(userp);
+        data->append(static_cast<char*>(ptr), size * nmemb);
+
+        return size * nmemb;
+    }
+
+public:
+    EmailConnector(std::string pipeid, ConnectorMode mode_, json const & json_descr):
+            Connector(pipeid, mode_, json_descr){
+        try{
+            authbundle_id_ = json_descr.at("authbundle_id").get<std::string>();
+            parse_authbundle();
+        }catch(json::exception){
+            throw std::runtime_error("authbundle_id cannot be null for email connector");
+        }
+
+        try{
+            address_ = json_descr.at("address").get<std::string>();
+        }catch(json::exception){
+            throw std::runtime_error("Adress cannot be null for email connector");
+        }
+
+        if(mode_ == ConnectorMode::OUT){
+            try{
+                smtp_server_ = json_descr.at("smtp_server").get<std::string>();
+            }catch(json::exception){
+                throw std::runtime_error("Smtp server cannot be null for email connector_out");
+            }
+
+            try{
+                smtp_port_ = json_descr.at("smtp_port").get<int>();
+            }catch(json::exception){
+                smtp_port_ = SMTP_PORT;
+            }
+        }else if(mode_ == ConnectorMode::IN){
+            try{
+                imap_server_ = json_descr.at("imap_server").get<std::string>();
+            }catch(json::exception){
+                throw std::runtime_error("Imap server cannot be null for email connector_in");
+            }
+
+            try{
+                imap_port_ = json_descr.at("imap_port").get<int>();
+            }catch(json::exception){
+                imap_port_ = IMAP_PORT;
+            }
+        }else{
+            throw std::runtime_error("Unsupported connector mode!");
+        }
+    }
+
     void send_email(const std::string& subject, const std::string& body) {
         upload_status upload_ctx;
         upload_ctx.lines_read = 0;
 
         std::vector<std::string> email_content = {
-            "To: " + to_ + "\r\n",
+            "To: " + address_ + "\r\n",
             "From: " + username_ + "\r\n" ,
             "Subject: " + subject + "\r\n" ,
             "\r\n",
@@ -143,7 +164,7 @@ public:
             struct curl_slist* recipients = nullptr;
 
             curl_easy_setopt(curl, CURLOPT_MAIL_FROM, ("<" + username_ + ">").c_str());
-            recipients = curl_slist_append(recipients, ("<" + to_ + ">").c_str());
+            recipients = curl_slist_append(recipients, ("<" + address_ + ">").c_str());
             curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
 
             curl_easy_setopt(curl, CURLOPT_READFUNCTION, payload_source);
@@ -163,14 +184,17 @@ public:
     }
 
     void connect()override{
-        std::cout << "EmailConnector connecting to SMTP server: " << smtp_server_ << std::endl;
-
         curl = curl_easy_init();
         if(!curl) {
             throw std::runtime_error("Failed to initialize CURL");
         }
 
-        std::string url = "smtp://" + smtp_server_ + ":" + std::to_string(smtp_port_);
+        std::string url;
+        if(mode_ == ConnectorMode::OUT){
+            url = "smtp://" + smtp_server_ + ":" + std::to_string(smtp_port_);
+        }else{
+            url = "imaps://" + imap_server_ + ":" + std::to_string(imap_port_) + "/INBOX/;UID=*/;SECTION=TEXT";
+        }
 
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
         curl_easy_setopt(curl, CURLOPT_USERNAME, username_.c_str());
@@ -193,10 +217,23 @@ public:
     }
 
     void do_send(MessageWrapper & msg_w)override{
-        std::string subject = "Message from M2E Bridge";
+        std::string subject = msg_w.msg().get_topic();
         std::string body = msg_w.msg().get_raw();
 
         send_email(subject, body);
+    }
+
+    Message do_receive()override{
+        std::string email_data;
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &email_data);
+
+        res = curl_easy_perform(curl);
+        if(res != CURLE_OK){
+            throw std::runtime_error("Failed to receive email: " + std::string(curl_easy_strerror(res)));
+        }
+
+        return Message(email_data, "");
     }
 
     void disconnect()override{
