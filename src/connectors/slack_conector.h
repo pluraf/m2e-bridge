99f31d1c42fc441c3f9d85b2cce8703c2e23a786
Konstantin Tyurin <konstantin@pluraf.com>
2025-01-17 21:47:12 +0100

    Remove IN mode of SlackConnector

--- src/connectors/slack_conector.h
+++ src/connectors/slack_conector.h
@@ -28,18 +28,7 @@ private:
             if(ab.service_type != ServiceType::SLACK){
                 throw std::runtime_error("Incompatiable authbundle service type");
             }
-            // Need different passwords for different connector modes
-            switch (mode_){
-                case ConnectorMode::IN:
-                    oauth_token_ = ab.password;
-                    break;
-                case ConnectorMode::OUT:
-                    webhook_url_ = ab.password;
-                    break;
-                default:
-                    throw std::runtime_error("Unsupported connector mode!");
-            }
-
+            webhook_url_ = ab.password;
         }else{
             throw std::runtime_error("Not able to retrieve authbundle!");
         }
@@ -66,11 +55,7 @@ public:
         }
 
         if(mode_ == ConnectorMode::IN){
-            try {
-                channel_id_ = json_descr.at("channel_id").get<std::string>();
-            }catch(json::exception&){
-                throw std::runtime_error("channel_id cannot be null for Slack connector_out");
-            }
+            throw std::runtime_error("SlackConnector does not support IN mode!");
         }
     }
 
@@ -109,52 +94,6 @@ public:
 
     void do_send(MessageWrapper& msg_w) override{
         send_message(msg_w.msg().get_raw());
-    }
-
-    Message do_receive()override{
-        std::string response_data;
-
-        // Prepare URL
-        std::string url = "https://slack.com/api/conversations.history";
-        std::string post_data = "{\"channel\":\"" + channel_id_ + "\", \"limit\": 1}";
-
-        // Set CURL options
-        curl_easy_setopt(curl_, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl_, CURLOPT_POST, 1L);
-        curl_easy_setopt(curl_, CURLOPT_POSTFIELDS, post_data.c_str());
-        curl_easy_setopt(curl_, CURLOPT_POSTFIELDSIZE, post_data.size());
-
-        struct curl_slist* headers = nullptr;
-        headers = curl_slist_append(headers, ("Authorization: Bearer " + oauth_token_).c_str());
-        headers = curl_slist_append(headers, "Content-Type: application/json");
-        curl_easy_setopt(curl_, CURLOPT_HTTPHEADER, headers);
-
-        curl_easy_setopt(curl_, CURLOPT_WRITEFUNCTION, write_callback);
-        curl_easy_setopt(curl_, CURLOPT_WRITEDATA, &response_data);
-
-        // Perform the request
-        CURLcode res = curl_easy_perform(curl_);
-        curl_slist_free_all(headers);
-
-        if(res != CURLE_OK){
-            throw std::runtime_error("Failed to fetch Slack messages: " + std::string(curl_easy_strerror(res)));
-        }
-
-        // Parse JSON response
-        json json_response = json::parse(response_data);
-        if(!json_response.contains("ok") || !json_response["ok"].get<bool>()){
-            throw std::runtime_error("Error fetching messages: " + json_response.dump());
-        }
-
-        // Extract the last message
-        std::string message_;
-        if(!json_response["messages"].empty()){
-            message_ = json_response["messages"][0].at("text");
-        }else{
-            message_ = "";
-        }
-
-        return Message(message_, "Slack");
     }
 
     void disconnect() override{
